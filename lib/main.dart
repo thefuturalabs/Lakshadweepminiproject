0d37555fb80c49be7278861c769798aa67ac944a
futuraflutter <fluttertrainer.futuralabs@gmail.com>
2024-03-15 15:44:29 +0530

    music player app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/provider/playlist_provider.dart';
+import 'package:music_player/provider/theme_provider.dart';
+import 'package:music_player/view/homepage.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => PlaylistProvider(),
+    )
+  ], child: const MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
---
+++ lib/view/homepage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/components/my_drawer.dart';
+import 'package:music_player/models/songes.dart';
+import 'package:music_player/provider/playlist_provider.dart';
+import 'package:music_player/view/song_page.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final PlaylistProvider playlistProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistProvider = Provider.of<PlaylistProvider>(context, listen: false);
+  }
+
+  void goToSong(int songIndex) {
+    playlistProvider.currentSongIndex = songIndex;
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SongPage(),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(title: const Text('P L A Y L I S T')),
+      drawer: const MyDrawer(),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) {
+          final List<Songs> playlist = value.playlist;
+
+          return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: (context, index) {
+              final Songs song = playlist[index];
+
+              return ListTile(
+                title: Text(
+                  song.songName,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.imagePath),
+                contentPadding: const EdgeInsets.all(10),
+                onTap: () {
+                  goToSong(index);
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
